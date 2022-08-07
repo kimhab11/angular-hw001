d217afefe6dc6848055022102817212900007831
keomhab <keomhab@gmail.com>
2022-08-07 16:39:17 +0700

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { CardContentComponent } from './card-content/card-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    SidebarComponent,
+    CardContentComponent
   ],
   imports: [
     BrowserModule,
