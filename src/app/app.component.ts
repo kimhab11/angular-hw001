224bd17798d156c9c195134b465f2de9117956d4
keomhab <keomhab@gmail.com>
2022-08-08 08:51:37 +0700

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,51 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-hw001';
+  
+  subject =  {
+    name: 'Angular',
+    description: `Angular is an open framework and
+  platform for creating Single Page Applications, written in TypeScript
+  and supported and developed by Google.`,
+    thumbnail: 'angular.svg'
+  };
+
+  subject_list = [
+    {
+      name: 'Angular',
+      description: `Angular is an open framework and
+    platform for creating Single Page Applications, written in TypeScript
+    and supported and developed by Google.`,
+      thumbnail: 'angular.svg'
+    },
+    {
+      name: 'TypeScript',
+      description: `TypeScript is a strongly typed, object oriented, compiled language.
+       It was designed by Anders Hejlsberg (designer of C#) at Microsoft.`,
+      thumbnail: 'typescript.svg'
+    },
+    {
+      name: 'Kotlin',
+      description: `Kotlin is a modern, trending programming language. Kotlin is easy to learn,
+      especially if you already know Java (it is 100% compatible with Java).`,
+      thumbnail: 'kotlin.svg'
+    },
+    {
+      name: 'Java',
+      description: `Java is a high-level, class-based, object-oriented programming language that is designed to
+       have as few implementation dependencies as possible.`,
+      thumbnail: 'java.svg'
+    },
+    {
+      name: 'JavaScript',
+      description: `JavaScript often abbreviated JS, is a programming language that is one of the core technologies
+       of the World Wide Web, alongside HTML and CSS.`,
+      thumbnail: 'javascript.svg'
+    },
+
+  ]
+
+  dataFromChild(data:any){
+    this.subject = data
+  }
 }
--- src/app/card-content/card-content.component.ts
+++ src/app/card-content/card-content.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-content',
@@ -8,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class CardContentComponent implements OnInit {
 
   constructor() { }
+
+  @Input() 
+  cardData!:any;
 
   ngOnInit(): void {
   }
